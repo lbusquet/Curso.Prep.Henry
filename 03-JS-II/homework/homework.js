008d00b04d45012b5d0d97b138fa7c2c3a2b3b90
lbusquet <113254988+lbusquet@users.noreply.github.com>
2022-09-16 09:56:16 -0300

    oficina 1

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -72,6 +72,8 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero<50 && numero>20) {return true}
+  else {return false}
 }
 
 function esEntero(numero) {
@@ -82,6 +84,8 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (numero %1==0) {return true}
+  else {return false}
 }
 
 function fizzBuzz(numero) {

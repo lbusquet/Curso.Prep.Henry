92c9f072e579e87d5693f14f05ecb4f10415aea7
lbusquet <113254988+lbusquet@users.noreply.github.com>
2022-09-20 08:02:05 -0300

    Preparing jsIV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -87,6 +87,8 @@ function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password = nuevaPassword
+  return objeto
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -94,6 +96,8 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+  usuario.amigo.push(nuevoAmigo)
+  return usuario
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -102,6 +106,8 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for (var i = 0; i < usuarios.length; i++) {usuarios[i].esPremium = true }
+  return usuarios
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -111,6 +117,9 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var suma = 0
+  for (var i = 0; i < usuario.posts.length; i++) {suma = suma + usuario.posts[i].likes}
+  return suma
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -123,6 +132,9 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+  producto.calcularPrecioDescuento = function() {return this.precio - (this.precio * this.porcentajeDeDescuento)}
+  return producto
+
 
 }
 
